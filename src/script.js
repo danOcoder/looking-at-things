b07940a2390e2130339d894773a679f4f080ea05
Dan O'Connor <dan@strata.ltd>
2022-03-20 09:02:42 -0400

    style: added normalize & the-new-css-reset stylesheets

--- src/script.js
+++ src/script.js
@@ -1,32 +1,8 @@
-import { createApi } from "unsplash-js";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-let { ACCESS_KEY, SECRET_KEY } = process.env;
-
-console.log(process.env.ACCESS_KEY);
-
-// on your node server
-const serverApi = createApi({
-  accessKey: process.env.ACCESS_KEY,
-  //...other fetch options
-});
-
-// non-feed example
-serverApi.search
-  .getUsers({
-    query: "cat",
-    page: 1,
-    perPage: 10,
-  })
-  .then((result) => {
-    if (result.errors) {
-      // handle error here
-      console.log("error occurred: ", result.errors[0]);
-    } else {
-      // handle success here
-      const photo = result.response;
-      console.log(photo);
-    }
-  });
+fetch("http://localhost:3000/", {
+  // method: "GET",
+  // headers: {
+  //   accept: "application/json",
+  // },
+})
+  .then((res) => res.json())
+  .then((res) => console.log("res", res.response));
