b7336e725b0a91327f395ce40aea773b49cc4939
Dan O'Connor <dan@strata.ltd>
2022-03-27 09:03:29 -0400

    feat: set up sass file structure

--- src/script.js
+++ src/script.js
@@ -1,8 +1,8 @@
-fetch("http://localhost:3000/", {
-  // method: "GET",
-  // headers: {
-  //   accept: "application/json",
-  // },
-})
-  .then((res) => res.json())
-  .then((res) => console.log("res", res.response));
+// fetch("http://localhost:3000/", {
+//   method: "GET",
+//   headers: {
+//     accept: "application/json",
+//   },
+// })
+//   .then((res) => res.json())
+//   .then((res) => console.log("res", res.response));
